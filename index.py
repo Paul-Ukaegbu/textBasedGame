42045f390028ec185a8d74845370d1ba31010f58
Paul Ukaegbu <paulukaegbu@devpapi.io>
2022-02-19 12:58:25 -0500

    First commit

---
+++ index.py
@@ -0,0 +1,40 @@
+from cgi import print_form
+
+
+roomAndWeapons = {
+    "Room1":"Sword",
+    "Room2":"Shield",
+    "Room3":"Helmet",
+    "Room4":"Body Armor",
+    "Room5":"Veggies",
+    "Room6":"Horse"
+}
+
+def roomOne():
+    return 0
+
+def roomTwo():
+    return 0
+
+def roomThree():
+    return 0
+
+def roomFour():
+    return 0
+
+def roomFive():
+    return 0
+
+def roomSix():
+    return 0
+
+def roomSeven():
+    return 0
+
+def roomEight():
+    return 0
+
+
+
+for room in roomAndWeapons:
+    print(roomAndWeapons[room])
