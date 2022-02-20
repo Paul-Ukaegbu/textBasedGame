1290e392ac68df8bccbd227efacfac5ce357e600
Paul Ukaegbu <paulukaegbu@devpapi.io>
2022-02-19 20:58:02 -0500

    added activities to rooms

--- index.py
+++ index.py
@@ -5,29 +5,36 @@ This exam will determine if you are worthy of acceptance into the school.
 You will be navigating 8 rooms, picking up all weapons and health 
 needed to defeat the monster that will be waiting for you at the final room. ''')
 
-roomThree()
-
 bagPack = []
 
 roomAndWeapons = {
     "Room1":"Sword",
     "Room2":"Shield",
     "Room4":"Helmet",
-    "Room4":"Body Armor",
+    "Room7":"Body Armor",
     "Room5":"Veggies",
     "Room6":"Time Amulet"
 }
 
 def roomOne():
     print()
-    print('''Hi welcome to the hall of Einstein!
-    In this room there is a Sword.''')
+    print('''Hi welcome to the hall of Einstein!In this room there is a Sword.''')
     getSword = input("Would you like to add this sword to your bag pack? ")
     if getSword  == 'Y':
-        bagPack.append(get)
-
-    #East and south
-    return 0
+        bagPack.append(roomAndWeapons["Room1"])
+        print(f"Your bag pack now has {bagPack}")
+        print()
+    
+    print("You can go east or you can go south. I'll recommend going east first ;)")
+    enterDirection = input("What direction would you like to go? ")
+    if enterDirection == "go east":
+        return roomSeven()
+    elif enterDirection == "go south":
+        return roomThree()
+    elif enterDirection == "go north" or "go west":
+        print("You can't go that direction.")
+    else:
+        print("Enter a valid direction.")
 
 def roomTwo():
     print('''Hi welcome to the hall of Faraday.''')
@@ -36,6 +43,7 @@ def roomTwo():
 
 def roomThree():
     count = 0
+    print()
     print('''Welcome to the main room. In this room you can go in any direction.''')
     
     readyToPlay = input("Are you ready to play? ")
@@ -53,7 +61,7 @@ def roomThree():
             print("Enter a valid room")
 
 def roomFour():
-    print('''Hi welcome to the wall of Nikola Tesla.''')
+    print('''Hi welcome to the hall of Nikola Tesla.''')
     #east
     return 0
 
@@ -68,15 +76,29 @@ def roomSix():
     return 0
 
 def roomSeven():
-    print('''Hi welcome to the wall of Einstein.''')
+    print()
+    print('''Hi welcome to the Hall of Dumbledore. In this room there is a Body Armor.''')
+    getSword = input("Would you like to add this body armor to your bag pack? ")
+    if getSword  == 'Y':
+        bagPack.append(roomAndWeapons["Room7"])
+        print(f"Your bag pack now has {bagPack}")
+        print()
+    
+    print("Only way to go now is West. Back to Einstein hall.")
+    enterDirection = input("What direction would you like to go? ")
+    if enterDirection == "go west":
+        return roomOne()
+    else:
+        print("There is no way or room to go except west")
+
     #west
-    return 0
 
 def roomEight():
     print('''Hi welcome to the wall of Einstein.''')
     #monster room
     return 0
 
+roomThree()
 
 
 # for room in roomAndWeapons:
